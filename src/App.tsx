6da4a67473530298a3e64a2275bf7fb3e394e499
Jacek jsec Glab <j.glab@dook.pro>
2022-03-15 12:50:02 +0100

    fix(deps): removed unused dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React from "react";
 import { createServer } from "miragejs";
 
 interface IEntry {
@@ -8,108 +8,107 @@ interface IEntry {
   breaksIntoSpans: boolean;
 }
 
+const res = {
+  text: "This is recursive entry",
+  subEntries: [
+    {
+      text: "This is sub-entry containing additional sub-entries:",
+      subEntries: [
+        {
+          text: "a) This is first internal sub-entry",
+          subEntries: null,
+          copyable: false,
+          breaksIntoSpans: false,
+        },
+        {
+          text: null,
+          subEntries: [
+            {
+              text: "b) This is second internal sub-entry",
+              subEntries: null,
+              copyable: false,
+              breaksIntoSpans: false,
+            },
+            {
+              text: " but with ",
+              subEntries: null,
+              copyable: false,
+              breaksIntoSpans: true,
+            },
+            {
+              text: "copyable element",
+              subEntries: null,
+              copyable: true,
+              breaksIntoSpans: true,
+            },
+          ],
+          copyable: false,
+          breaksIntoSpans: true,
+        },
+      ],
+      copyable: false,
+      breaksIntoSpans: false,
+    },
+    {
+      text: null,
+      subEntries: [
+        {
+          text: "This sub-entry",
+          subEntries: null,
+          copyable: false,
+          breaksIntoSpans: false,
+        },
+        {
+          text: " without copyable element",
+          subEntries: null,
+          copyable: false,
+          breaksIntoSpans: true,
+        },
+      ],
+      copyable: false,
+      breaksIntoSpans: true,
+    },
+    {
+      text: null,
+      subEntries: [
+        {
+          text: "This is another sub-entry, but it has ",
+          subEntries: null,
+          copyable: false,
+          breaksIntoSpans: false,
+        },
+        {
+          text: "a copyable element",
+          subEntries: null,
+          copyable: true,
+          breaksIntoSpans: false,
+        },
+      ],
+      copyable: false,
+      breaksIntoSpans: true,
+    },
+    {
+      text: "This sub-entry don't have any sub-entries",
+      subEntries: null,
+      copyable: false,
+      breaksIntoSpans: false,
+    },
+  ],
+  copyable: false,
+  breaksIntoSpans: false,
+};
+
 createServer({
   routes() {
     this.namespace = "api";
-
-    this.get("/entry", () => {
-      return {
-        text: "This is recursive entry",
-        subEntries: [
-          {
-            text: "This is sub-entry containing additional sub-entries:",
-            subEntries: [
-              {
-                text: "a) This is first internal sub-entry",
-                subEntries: null,
-                copyable: false,
-                breaksIntoSpans: false,
-              },
-              {
-                text: null,
-                subEntries: [
-                  {
-                    text: "b) This is second internal sub-entry",
-                    subEntries: null,
-                    copyable: false,
-                    breaksIntoSpans: false,
-                  },
-                  {
-                    text: " but with ",
-                    subEntries: null,
-                    copyable: false,
-                    breaksIntoSpans: true,
-                  },
-                  {
-                    text: "copyable element",
-                    subEntries: null,
-                    copyable: true,
-                    breaksIntoSpans: true,
-                  },
-                ],
-                copyable: false,
-                breaksIntoSpans: true,
-              },
-            ],
-            copyable: false,
-            breaksIntoSpans: false,
-          },
-          {
-            text: null,
-            subEntries: [
-              {
-                text: "This sub-entry",
-                subEntries: null,
-                copyable: false,
-                breaksIntoSpans: false,
-              },
-              {
-                text: " without copyable element",
-                subEntries: null,
-                copyable: false,
-                breaksIntoSpans: true,
-              },
-            ],
-            copyable: false,
-            breaksIntoSpans: true,
-          },
-          {
-            text: null,
-            subEntries: [
-              {
-                text: "This is another sub-entry, but it has ",
-                subEntries: null,
-                copyable: false,
-                breaksIntoSpans: false,
-              },
-              {
-                text: "a copyable element",
-                subEntries: null,
-                copyable: true,
-                breaksIntoSpans: false,
-              },
-            ],
-            copyable: false,
-            breaksIntoSpans: true,
-          },
-          {
-            text: "This sub-entry don't have any sub-entries",
-            subEntries: null,
-            copyable: false,
-            breaksIntoSpans: false,
-          },
-        ],
-        copyable: false,
-        breaksIntoSpans: false,
-      };
-    });
+    this.get("/entry", () => res);
   },
 });
 
 function App() {
-  const [response, setResponse] = useState<IEntry | null>(null);
+  const [response, setResponse] = React.useState<IEntry | null>(null);
 
-  useEffect(() => {
+  React.useEffect(() => {
     fetch("/api/entry/")
       .then((res) => res.json())
       .then((result) => {
